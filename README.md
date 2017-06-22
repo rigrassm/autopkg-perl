1bbed5fe513706ea04b4533b7878fd3ea411cb78
Ricky Grassmuck <rigrassm@gmail.com>
2017-06-21 23:02:06 -0500

    Initial commit for the Solus Perl module package creation utility

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
 # autopkg-perl
-Utility for automatically generating eopkg's for perl modules using the MetaCpan API for sourcing the necessary packaging data
+Utility for automatically generating eopkg's for perl modules using the [MetaCPAN](https://fastapi.metacpan.org/) API for sourcing the necessary packaging data
+
+# Usage
+In it's current form it's just grabbing some Perl module data that will be used in the future for generating eopkgs
+automatically.
+
+```[]
+python3 autopkg-perl
+```
+
+You can edit the list of Perl modules in the `main()` function to test against different Perl modules. The names must match
+the name of the module listed on MetaCPAN
+
+## Example Output
+**/usr/bin/python3.5 /home/rigrassm/Development/github/autopkg-perl/autopkg-perl.py**
+
+```
+============================================================
+HTTP-Tiny - Version 0.070
+============================================================
+
+{'configure': {'requires': {'perl-extutils-makemaker': '6.17',
+                            'perl-perl': '5.006'}},
+ 'runtime': {'recommends': {'perl-http-cookiejar': '0.001',
+                            'perl-io-socket-ip': '0.32',
+                            'perl-io-socket-ssl': '1.42',
+                            'perl-mozilla-ca': '20160104',
+                            'perl-net-ssleay': '1.49'},
+             'requires': {'perl-bytes': '0',
+                          'perl-carp': '0',
+                          'perl-fcntl': '0',
+                          'perl-io-socket': '0',
+                          'perl-mime-base64': '0',
+                          'perl-perl': '5.006',
+                          'perl-socket': '0',
+                          'perl-strict': '0',
+                          'perl-time-local': '0',
+                          'perl-warnings': '0'}}}
+
+```
+
+The requirements are converted to match the Solus naming scheme for perl
+modules provided by it's repositories. Right now, this conversion is not
+reversible due to the names being converted to lower case. Future work
+will be done to provide a sane way for preserving the original name.
+
+## Example
+###### Perl Syntax
+- HTTP::Tiny
+
+###### Solus Package Name
+* perl-http-tiny
+
+## Links
+- [Solus-Project - About](https://solus-project.com/solus/about)
+- [Solus-Project - Packaging Guidelines](https://solus-project.com/articles/packaging/https://solus-project.com/solus/about)
+- [MetaCPAN - API v1 Documentation](https://fastapi.metacpan.org/)
---
+++ autopkg-perl.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python3
+
+import json
+import requests
+import pprint
+
+
+def get_module_info(perl_module, base='https://fastapi.metacpan.org'):
+    search_url = "{base}/release/{mod_name}".format(base=base, mod_name=perl_module)
+    result = requests.get(search_url)
+    return result.json()
+
+
+def post_search(base, q):
+    # Leaving this function here but I don't think it will be used for API calls as the data returned is in a less than
+    # optimal format for our use cases.
+
+    search_url = "{base}/{ep}".format(base=base, ep="v1/release/_search")
+    result = requests.get(search_url, data=json.dumps(q))
+    return result.json()
+
+
+def strip_dbl_colons(text):
+    if type(text) is dict:
+        is_dict = True
+        mod_names_string = json.dumps(text)
+    else:
+        is_dict = False
+        mod_names_string = text
+    stripped = str(mod_names_string).replace('::', '-')
+
+    return json.loads(stripped) if is_dict else stripped
+
+
+def get_requires(mod_data, recommends=False, suggests=False):
+    # This function is a little more in depth then the name would indicate, it takes the modules configure and runtime
+    # dependencies and returns them in the Solus Package naming scheme. This will make generating package.yml files a
+    # fairly smooth process.
+
+    pre_reqs = mod_data["prereqs"]
+    mod_name = mod_data["name"]
+
+    req_types_list = ["configure", "runtime"]
+    req_level = ["requires"]
+
+    if recommends:
+        req_level.append("recommends")
+    if suggests:
+        req_level.append("suggests")
+
+    requires = {mod_name: {"reqs": {"configure": {}, "runtime": {}}}}
+
+    for item in req_types_list:
+        for level in req_level:
+            if level in pre_reqs[item]:
+                stripped = strip_dbl_colons(pre_reqs[item][level])
+                requires[mod_name]["reqs"][item][level] = {gen_eopkg_name(k): v for (k, v) in stripped.items()}
+
+    return requires
+
+
+def gen_eopkg_name(module_name):
+    # Takes a Perl Module in the Perl::Module::Format and returns a compliant Solus Perl module package name
+
+    prefix = 'perl-'
+    return "{prefix}{mod_name}".format(prefix=prefix, mod_name=strip_dbl_colons(module_name)).lower()
+
+
+def _wrap_print(header, body):
+    pp = pprint.PrettyPrinter(width=30)
+    print("=" * 60)
+    print(header)
+    print("=" * 60)
+    pp.pprint(body)
+    print("")
+
+
+def main(modules):
+    base_url = 'https://fastapi.metacpan.org'
+
+    for mod in modules:
+        data = get_module_info(mod, base_url)
+        requirements = get_requires(mod_data=data["metadata"], recommends=True)
+        _wrap_print(header="{} - Version {}".format(mod, data['version']), body=requirements[mod]["reqs"])
+
+
+if __name__ == '__main__':
+    test_modules = ["HTTP-Tiny", "Moose", "Try-Tiny"]
+    main(test_modules)
